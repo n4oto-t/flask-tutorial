ad44ef7895796332cb3bfb4c0df64c89e0eb9d3f
N4oto-T <h13.only57@gmail.com>
2024-01-11 21:22:31 +0900

    create and register blueprint

---
+++ flaskr/auth.py
@@ -0,0 +1,21 @@
+import functools
+
+
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+from flaskr.db import get_db
+
+
+bp = Blueprint("auto", __name__, url_prefix="/auth")
